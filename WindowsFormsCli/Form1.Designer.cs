304b50adb5dbcdd6407b270e8d48c31fd92ae974
Tamás Mikola <mtamas@recrovit.hu>
2020-11-10 15:41:09 +0100

    Add-WindowsFormsCli

---
+++ WindowsFormsCli/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindowsFormsCli
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonFill = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.buttonGet = new System.Windows.Forms.Button();
+            this.buttonSet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(22, 26);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(245, 349);
+            this.listBox1.TabIndex = 0;
+            // 
+            // buttonFill
+            // 
+            this.buttonFill.Location = new System.Drawing.Point(314, 58);
+            this.buttonFill.Name = "buttonFill";
+            this.buttonFill.Size = new System.Drawing.Size(75, 23);
+            this.buttonFill.TabIndex = 1;
+            this.buttonFill.Text = "Fill";
+            this.buttonFill.UseVisualStyleBackColor = true;
+            this.buttonFill.Click += new System.EventHandler(this.buttonFill_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(314, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "URL";
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point(348, 23);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(212, 23);
+            this.textBoxURL.TabIndex = 3;
+            // 
+            // buttonGet
+            // 
+            this.buttonGet.Location = new System.Drawing.Point(314, 88);
+            this.buttonGet.Name = "buttonGet";
+            this.buttonGet.Size = new System.Drawing.Size(75, 23);
+            this.buttonGet.TabIndex = 4;
+            this.buttonGet.Text = "Get";
+            this.buttonGet.UseVisualStyleBackColor = true;
+            this.buttonGet.Click += new System.EventHandler(this.buttonGet_Click);
+            // 
+            // buttonSet
+            // 
+            this.buttonSet.Location = new System.Drawing.Point(314, 118);
+            this.buttonSet.Name = "buttonSet";
+            this.buttonSet.Size = new System.Drawing.Size(75, 23);
+            this.buttonSet.TabIndex = 5;
+            this.buttonSet.Text = "Set";
+            this.buttonSet.UseVisualStyleBackColor = true;
+            this.buttonSet.Click += new System.EventHandler(this.buttonSet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSet);
+            this.Controls.Add(this.buttonGet);
+            this.Controls.Add(this.textBoxURL);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonFill);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonFill;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.Button buttonGet;
+        private System.Windows.Forms.Button buttonSet;
+    }
+}
+
---
+++ WindowsFormsCli/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace WindowsFormsCli
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.textBoxURL.Text = "https://localhost:5001";
+        }
+
+        private HttpClient CreateClient()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(this.textBoxURL.Text);
+            return client;
+        }
+
+        private async void buttonFill_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (HttpClient client = CreateClient())
+                {
+                    Cursor.Current = Cursors.WaitCursor;
+                    var response = await client.GetAsync("/api/dokumentumok");
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        var res = await response.Content.ReadAsStringAsync();
+                        var files = (JsonSerializer.Deserialize(res, typeof(string[])) as string[]).ToList();
+                        this.listBox1.DataSource = files;
+                    }
+                    else
+                    {
+                        MessageBox.Show(await response.Content.ReadAsStringAsync());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private async void buttonGet_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                try
+                {
+                    using (HttpClient client = CreateClient())
+                    {
+                        Cursor.Current = Cursors.WaitCursor;
+                        var name = HttpUtility.UrlEncode(listBox1.SelectedItem.ToString());
+                        var response = await client.GetAsync($"/api/dokumentumok/{name}");
+                        var res = await response.Content.ReadAsStringAsync();
+                        if (response.StatusCode == HttpStatusCode.OK)
+                        {
+                            var data = Convert.FromBase64String(res);
+                            if (data.Length > 1E4)
+                            {
+                                res = "A file túl nagy megjeleníteni.";
+                            }
+                            else
+                            {
+                                res = Encoding.UTF8.GetString(data);
+                            }
+                        }
+                        MessageBox.Show(res);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private async void buttonSet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    var fileContent = string.Empty;
+                    var filePath = string.Empty;
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        Cursor.Current = Cursors.WaitCursor;
+                        var data = await System.IO.File.ReadAllBytesAsync(openFileDialog.FileName);
+                        using (HttpClient client = CreateClient())
+                        {
+                            string body = Convert.ToBase64String(data);
+                            var content = new StringContent(body);
+                            var name = HttpUtility.UrlEncode(Path.GetFileName(openFileDialog.FileName));
+                            var response = await client.PostAsync($"/api/dokumentumok/{name}", content);
+                            if (response.StatusCode != HttpStatusCode.OK)
+                            {
+                                var err = await response.Content.ReadAsStringAsync();
+                                MessageBox.Show(err);
+                            }
+                            else
+                            {
+                                MessageBox.Show("Upload OK");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
